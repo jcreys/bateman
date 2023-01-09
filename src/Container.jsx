4d3d398c93677a137bbfed69e0876c386914ed24
Joshua Reyes <reyes.j15@gmail.com>
2023-01-09 03:05:44 -0500

    added preview feature

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,12 +1,44 @@
 export default function Container(props) {
+  console.log(props.jobTitle);
   return (
     <div className="bg-white px-4 py-5 shadow sm:rounded-lg sm:p-6">
-      <span>{props.firstName}</span>
-      <br></br><span>{props.jobTitle}</span>
-      <br></br><span>{props.department}</span>
-      <br></br><span>{props.companyName}</span>
-      <br></br><span>{props.email}</span>
+      <table cellPadding={0} cellSpacing={0} className={"signature"}>
+        <tbody>
+          <tr>
+            <td rowSpan={5}>
+              <img className={"main-image"} src="/assets/pngegg.png" alt={""} />
+            </td>
+            <td rowSpan={5}>
+              <div className={"vertical-line"} />
+            </td>
+            <td>{props.firstName}</td>
+          </tr>
+          <tr>
+            <td colSpan={3}>{props.jobTitle}</td>
+          </tr>
+          <tr>
+            <td colSpan={3}>Email: {props.email}</td>
+          </tr>
+          <tr>
+            <td colSpan={3}>Company: {props.companyName}</td>
+          </tr>
+          <tr>
+            <td colSpan={3}>
+              <div className={"social-logos-frame"}>
+                <a href={"https://twitter.com/"}>
+                  <img src="/assets/twitter.png" alt={""} />
+                </a>
+                <a href={"https://www.linkedin.com/"}>
+                  <img src="/assets/linkedin.png" alt={""} />
+                </a>
+                <a href={"https://www.facebook.com/"}>
+                  <img src="/assets/facebook.png" alt={""} />
+                </a>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-
   );
 }
